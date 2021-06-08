1c76872a10da1f87d501313245e9e79c61662aa8
Anatoliy_Iliev <vekok12@gmail.com>
2021-06-08 17:30:20 +0300

    hw13

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -2,7 +2,6 @@ export default function getRefs(){
     return {
         searchForm: document.querySelector('.js-search-form'),
         articlesContainer: document.querySelector('.js-gallary-container'),
-        loadMoreBtn: document.querySelector('[data-action="load-more"]'),
-    
+        // loadMoreBtn: document.querySelector('[data-action="load-more"]'),
     };
 }
--- src/js/index.js
+++ src/js/index.js
@@ -2,26 +2,51 @@ import '../sass/main.scss';
 import galleryCards from '../templates/gallery-cards.hbs';
 import GalleryApiService from './apiService.js';
 import getRefs from './get-refs.js';
+import LoadMoreBtn from './load-more-batton';
 
 const refs = getRefs();
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
 const newGalleryApiService = new GalleryApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+// refs.loadMoreBtn.addEventListener('click', onLoadMore);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
 
 function onSearch(e) {
   e.preventDefault();
-  clearContainer();
+  
+
+  loadMoreBtn.show();
+  loadMoreBtn.disable(); 
     
   newGalleryApiService.query = e.currentTarget.elements.query.value;  
   newGalleryApiService.resetPage();
   newGalleryApiService.apiService()
-      .then(buildListMarkup)
+      .then(gallery => {
+        clearContainer(); 
+        buildListMarkup(gallery); // тут пересмотреть
+        
+        
+        loadMoreBtn.disable();
+      })
       .catch('error');
+
+       
 }
 
 function buildListMarkup(gallery) {
   refs.articlesContainer.insertAdjacentHTML('beforeend', galleryCards(gallery))
+  scroll();
+}
+
+function scroll(){
+  refs.loadMoreBtn.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
 }
 
 function clearContainer() {
@@ -31,8 +56,9 @@ function clearContainer() {
 function onLoadMore() {
   newGalleryApiService.apiService()
       .then(buildListMarkup)
-      .catch('error');  
+      .catch('error');   
 }
+
 
 
 
